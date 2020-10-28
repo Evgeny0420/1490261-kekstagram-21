2603477b4b51797d540a3532e641ccdad2c3e471
Евгений <evgeniy_karpov91@inbox.ru>
2020-10-29 00:23:09 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ const MESSAGE = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 const NAME = ['Катя', 'Женя.', 'Вова', 'Олеся', 'Кира', 'Костя'];
+const TEXT = 'Я сидел в березовой роще осенью, около половины сентября. С самого утра перепадал мелкий дождик, сменяемый по временам теплым солнечным сиянием; была непостоянная погода. Небо то всё заволакивалось рыхлыми белыми облаками, то вдруг местами расчищалось на мгновенье, и тогда из-за раздвинутых туч показывалась лазурь, ясная и ласковая, как прекрасный глаз.';
 
 // функция рандомного числа
 const randomNumber = function (min, max) {
@@ -27,22 +28,31 @@ const randomArray = function (arr) {
 const randomAvatar = function () {
   return `img/avatar-${randomNumber(MIN_NUMBER, MAX_AVATAR)}.svg`;
 };
+
 // создание коментария
 const сomments = [];
 for (let i = 0; i < 3; i++) {
-  const newComment = {
+  сomments.push({
     avatar: randomAvatar(),
     message: randomArray(MESSAGE),
     name: randomArray(NAME)
-  };
-  сomments.push(newComment);
+  });
 }
+
+/* const randomLorem = function () {
+  return TEXT.substr(0, randomNumber(10, TEXT.length));
+}; */
+
+const randomLorem = function () {
+  const a = TEXT.split('');
+  return a.slice(0, randomNumber(4, a.length)).join('');
+};
 // создание массива с фотографиями, лайками и комментариями
 const photos = [];
 for (let i = 1; i <= PHOTO_MAX_NUMBER; i++) {
   const photo = {
     url: `photos/${i}.jpg`,
-    description: 'фото',
+    description: randomLorem(),
     likes: randomNumber(MIN_LIKES, MAX_LIKES),
     comments: сomments.length
   };
