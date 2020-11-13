4b80fc86f83a0104a90aa3a50ae3b108446c729a
Евгений <evgeniy_karpov91@inbox.ru>
2020-11-13 19:55:47 +0300

    module-task1

--- js/bigPhotos.js
+++ js/bigPhotos.js
@@ -8,7 +8,6 @@
   const bigPhoto = document.querySelector('.big-picture__img img');
   const likes = document.querySelector('.likes-count');
   const pictureImg = document.querySelectorAll('.picture__img');
-  const pictureLink = document.querySelectorAll('.picture');
   const socialComment = document.querySelectorAll('.social__comment');
   const CancelX = document.querySelector('.big-picture__cancel');
   const socialCommentCount = document.querySelector('.social__comment-count');
@@ -24,29 +23,32 @@
     bigPicture.classList.add('hidden');
     window.popup.body.classList.remove('modal-open');
   };
-  for (let i = 0; i < pictureImg.length; i++) {
-    pictureLink[i].addEventListener('click', function () {
-      openPicture();
-      bigPhoto.src = window.main.photos[i].url;
-      likes.textContent = window.main.photos[i].likes;
-      commentsCount.textContent = 10;
-      socialCaption.textContent = window.main.photos[i].description;
-      for (let j = 0; j < socialComment.length; j++) {
-        const socialPicture = socialComment[j].querySelector('.social__picture');
-        const socialText = socialComment[j].querySelector('.social__text');
-        socialPicture.src = window.main.photos[i].comments[j].avatar;
-        socialPicture.alt = window.main.photos[i].comments[j].name;
-        socialText.textContent = window.main.photos[i].comments[j].message;
-      }
-    });
-    pictureLink[i].addEventListener('keydown', function (evt) {
-      window.util.isEnterEvent(evt, openPicture);
-    });
+  window.bigPhotos = function (photos) {
+    const pictureLink = document.querySelectorAll('.picture');
+    for (let i = 0; i < pictureLink.length; i++) {
+      pictureLink[i].addEventListener('click', function () {
+        openPicture();
+        bigPhoto.src = photos[i].url;
+        likes.textContent = photos[i].likes;
+        commentsCount.textContent = 10;
+        socialCaption.textContent = photos[i].description;
+        for (let j = 0; j < socialComment.length; j++) {
+          const socialPicture = socialComment[j].querySelector('.social__picture');
+          const socialText = socialComment[j].querySelector('.social__text');
+          socialPicture.src = photos[i].comments[j].avatar;
+          socialPicture.alt = photos[i].comments[j].name;
+          socialText.textContent = photos[i].comments[j].message;
+        }
+      });
+      pictureLink[i].addEventListener('keydown', function (evt) {
+        window.util.isEnterEvent(evt, openPicture);
+      });
+    }
     CancelX.addEventListener('click', function () {
       cancelPicture();
     });
     document.addEventListener('keydown', function (evt) {
       window.util.isEscEvent(evt, cancelPicture);
     });
-  }
+  };
 })();
