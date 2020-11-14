7c215c059354a4db3a1f1061262c5cc71a495d10
Евгений <evgeniy_karpov91@inbox.ru>
2020-11-14 20:33:53 +0300

    module6-task2

--- js/random.js
+++ js/random.js
@@ -1,9 +1,9 @@
 'use strict';
 
 (function () {
-  window.random = {
-    randomNumber: function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  };
+  // window.random = {
+  //   randomNumber: function (min, max) {
+  //     return Math.floor(Math.random() * (max - min + 1)) + min;
+  //   }
+  // };
 })();
